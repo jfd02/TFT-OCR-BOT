dec0cad17d3e26aaef4ba0d3ed646c31fd511b3b
jfd02 <jerrydurkin02@gmail.com>
2022-01-28 13:01:58 -0500

    Ver 2.1.0
    Fixed a possible crash from Tacticians Crown check, added click location randomization

--- mk_functions.py
+++ mk_functions.py
@@ -1,20 +1,24 @@
 import pydirectinput
+import random
 
 
 def left_click(coords_tuple):
-    pydirectinput.moveTo(coords_tuple[0], coords_tuple[1])
+    offset = random.randint(-3, 3)
+    pydirectinput.moveTo(coords_tuple[0] - offset, coords_tuple[1] - offset)
     pydirectinput.mouseDown()
     pydirectinput.mouseUp()
 
 
 def right_click(coords_tuple):
-    pydirectinput.moveTo(coords_tuple[0], coords_tuple[1])
+    offset = random.randint(-3, 3)
+    pydirectinput.moveTo(coords_tuple[0]- offset, coords_tuple[1] - offset)
     pydirectinput.mouseDown(button='right')
     pydirectinput.mouseUp(button='right')
 
 
 def press_e(coords_tuple):
-    pydirectinput.moveTo(coords_tuple[0], coords_tuple[1])
+    offset = random.randint(-3, 3)
+    pydirectinput.moveTo(coords_tuple[0] - offset, coords_tuple[1] - offset)
     pydirectinput.press("e")
 
 
