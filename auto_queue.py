783a2c5c62769468b64b71d123925aa6d284e403
jfd02 <jerrydurkin02@gmail.com>
2022-02-14 14:25:05 -0500

    Small auto queue bugfix
    Potential bugfix & catch exceptions properly. Still needs a revamp to make sure queue can never get stuck and can recover if it does.

--- auto_queue.py
+++ auto_queue.py
@@ -1,3 +1,4 @@
+from http import server
 import subprocess
 import re
 from requests.auth import HTTPBasicAuth
@@ -11,54 +12,54 @@ import numpy as np
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-def create_lobby(message_queue, server_url, remoting_auth_token):
+def create_lobby(message_queue, client_info):
     payload = {"queueId": 1090}  # Ranked TFT is 1100
     payload = json.dumps(payload)
-
     try:
-        status = requests.post(server_url + "/lol-lobby/v2/lobby/", payload,
-                               auth=HTTPBasicAuth('riot', remoting_auth_token), verify=False)
+        status = requests.post(client_info[1] + "/lol-lobby/v2/lobby/", payload,
+                            auth=HTTPBasicAuth('riot', client_info[0]), verify=False)
         if status.status_code == 200:
-            message_queue.put(("CONSOLE", "Creating TFT lobby."))
-            return 200
+            return True
         else:
-            message_queue.put(("CONSOLE", "Failed to create a lobby, trying again..."))
-            return 400
-    except:  # I don't know what exception this throws
-        message_queue.put(("CONSOLE", "Failed to create a lobby, trying again..."))
-        return 400
+            return False
+    except ConnectionError:
+        return False
 
 
-def start_queue(message_queue, server_url, remoting_auth_token):
+def start_queue(message_queue, client_info):
     try:
-        status = requests.post(server_url + "/lol-lobby/v2/lobby/matchmaking/search",
-                               auth=HTTPBasicAuth('riot', remoting_auth_token), verify=False)
+        status =requests.post(client_info[1] + "/lol-lobby/v2/lobby/matchmaking/search",
+                            auth=HTTPBasicAuth('riot', client_info[0]), verify=False)
         if status.status_code == 204:
-            message_queue.put(("CONSOLE", "Starting queue"))
-            return 204
+            return True
         else:
-            message_queue.put(("CONSOLE", "Queue start failed, trying again..."))
-            return 400
-    except:  # Same as above
-        message_queue.put(("CONSOLE", "Queue start failed, trying again..."))
-        return 400
+            return False
+    except ConnectionError:
+        return False
 
 
-def accept_queue(server_url, remoting_auth_token):
-    requests.post(server_url + "/lol-matchmaking/v1/ready-check/accept",
-                  auth=HTTPBasicAuth('riot', remoting_auth_token), verify=False)
+def accept_queue(client_info):
+    requests.post(client_info[1] + "/lol-matchmaking/v1/ready-check/accept",
+                  auth=HTTPBasicAuth('riot', client_info[0]), verify=False)
 
 
-def change_arena_skin(server_url, remoting_auth_token):
-    requests.delete(server_url + "/lol-cosmetics/v1/selection/tft-map-skin",
-                    auth=HTTPBasicAuth('riot', remoting_auth_token), verify=False)
+def change_arena_skin(client_info):
+    try:
+        status = requests.delete(client_info[1] + "/lol-cosmetics/v1/selection/tft-map-skin",
+                                auth=HTTPBasicAuth('riot', client_info[0]), verify=False)
+        if status.status_code == 204:
+            return True
+        else:
+            return False
+    except ConnectionError:
+        return False
 
 
-def queue(message_queue):
+def get_client(message_queue):
     message_queue.put(("CONSOLE", "[Auto Queue]"))
     results = str(subprocess.run(['wmic', 'PROCESS', 'WHERE', "name= 'LeagueClientUx.exe'", 'GET', 'commandline'],
                                  capture_output=True))
-    while "No Instance(s) Available." in results:  # Means client is not open
+    while "--app-port" not in results:  # Means client is not open
         message_queue.put(("CONSOLE", "Client not open! Trying again in 10 seconds..."))
         sleep(10)
         results = str(subprocess.run(['wmic', 'PROCESS', 'WHERE', "name= 'LeagueClientUx.exe'", 'GET', 'commandline'],
@@ -68,18 +69,23 @@ def queue(message_queue):
     app_port = re.search("--app-port=([0-9]*)", results)[1]
     remoting_auth_token = re.search("--remoting-auth-token=([\w-]*)", results)[1]
     server_url = f"https://127.0.0.1:{app_port}"
+    return (remoting_auth_token, server_url)
+    
 
-    while create_lobby(message_queue, server_url, remoting_auth_token) != 200:
+def queue(message_queue):
+    client_info = get_client(message_queue)
+    while create_lobby(message_queue, client_info) != True:
         sleep(3)
 
-    change_arena_skin(server_url, remoting_auth_token)
+    change_arena_skin(client_info)
 
-    sleep(5)  # This should be done differently but im done messing with the LCU
-    while start_queue(message_queue, server_url, remoting_auth_token) != 204:
+    sleep(5) 
+    while start_queue(message_queue, client_info) != True:
         sleep(3)
+
     in_queue = True
     while in_queue:
-        accept_queue(server_url, remoting_auth_token)
+        accept_queue(client_info)
         in_game = ImageGrab.grab(bbox=(19, 10, 38, 28))
         array = np.array(in_game)
         if (np.abs(array - (27, 79, 66)) <= 2).all(
@@ -87,3 +93,4 @@ def queue(message_queue):
             in_queue = False
         sleep(1)
     message_queue.put(("CONSOLE", "Loading screen found! Waiting for round 1-1"))
+
