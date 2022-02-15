5e5b612428e6776cb5da351c07d25da069cef2e8
jfd02 <jerrydurkin02@gmail.com>
2022-02-15 16:19:04 -0500

    Update settings example
    Make settings example more clear

--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
 forfeit = False
 forfeit_time = 600 # time in seconds
-league_client_path = None # Replace None with your path. EX: 'C:\Riot Games\League of Legends'
+league_client_path = None
+# Replace None with your path.
+# The correct path is the folder that contains LeagueClient.exe, the path must be a string so wrap it in '', also include double \\ between directories
+# EX:
+# league_client_path = 'C:\\Riot Games\\League of Legends'
