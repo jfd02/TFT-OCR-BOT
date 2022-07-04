1d54c2f6d5542e5c83fbf1dfca53c8f292164983
jfd02 <jerrydurkin02@gmail.com>
2022-07-04 13:07:52 -0400

    Making vector classes
    Vec2 / Vec4

---
+++ vec2.py
@@ -0,0 +1,17 @@
+
+class vec2:
+    screen_x_offset = 0
+    screen_y_offset = 0
+
+    def __init__(self, x, y, use_screen_offset: bool = True):
+        self.x = x
+        self.y = y
+        self.use_screen_offset = use_screen_offset
+
+    def get_coords(self) -> tuple:
+        if self.use_screen_offset:
+            print(self.x + vec2.screen_x_offset, self.y + vec2.screen_y_offset)
+            return (self.x + vec2.screen_x_offset, self.y + vec2.screen_y_offset)
+        else:
+            print(self.x, self.y)
+            return (self.x, self.y)
