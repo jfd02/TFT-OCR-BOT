e195cdf524172a055e3d7115c952bf82b764a5e2
jfd02 <jerrydurkin02@gmail.com>
2022-07-23 19:14:53 -0400

    Remove console overlay and implement print statements

--- auto_queue.py
+++ auto_queue.py
@@ -11,29 +11,27 @@ import settings
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-def create_lobby(message_queue, client_info):
+def create_lobby(client_info):
     payload = {"queueId": 1090}  # Ranked TFT is 1100
     payload = json.dumps(payload)
     try:
         status = requests.post(client_info[1] + "/lol-lobby/v2/lobby/", payload,
                             auth=HTTPBasicAuth('riot', client_info[0]), verify=False)
         if status.status_code == 200:
-            message_queue.put(("CONSOLE", "Creating lobby"))
+            print("  Creating lobby")
             return True
-        else:
-            return False
+        return False
     except ConnectionError:
         return False
 
-def start_queue(message_queue, client_info):
+def start_queue(client_info):
     try:
         status = requests.post(client_info[1] + "/lol-lobby/v2/lobby/matchmaking/search",
                             auth=HTTPBasicAuth('riot', client_info[0]), verify=False)
         if status.status_code == 204:
-            message_queue.put(("CONSOLE", "Starting queue"))
+            print("  Starting queue")
             return True
-        else:
-            return False
+        return False
     except ConnectionError:
         return False
 
@@ -58,20 +56,19 @@ def accept_queue(client_info):
     requests.post(client_info[1] + "/lol-matchmaking/v1/ready-check/accept",
                   auth=HTTPBasicAuth('riot', client_info[0]), verify=False)
 
-def change_arena_skin(message_queue, client_info):
+def change_arena_skin(client_info):
     try:
         status = requests.delete(client_info[1] + "/lol-cosmetics/v1/selection/tft-map-skin",
                                 auth=HTTPBasicAuth('riot', client_info[0]), verify=False)
         if status.status_code == 204:
-            message_queue.put(("CONSOLE", "Changed arena skin to default"))
+            print("  Changed arena skin to default")
             return True
-        else:
-            return False
+        return False
     except ConnectionError:
         return False
 
-def get_client(message_queue):
-    message_queue.put(("CONSOLE", "[Auto Queue]"))
+def get_client():
+    print("\n\n[Auto Queue]")
     file_path = settings.LEAGUE_CLIENT_PATH + "\\lockfile"
     got_lock_file = False
     while got_lock_file is False:
@@ -82,33 +79,33 @@ def get_client(message_queue):
             server_url = f"https://127.0.0.1:{app_port}"
             got_lock_file = True
         except IOError:
-            message_queue.put(("CONSOLE", "Client not open! Trying again in 10 seconds."))
+            print("  Client not open! Trying again in 10 seconds.")
             sleep(10)
-    message_queue.put(("CONSOLE", "Client found"))
+    print("  Client found")
     return (remoting_auth_token, server_url)
 
-def queue(message_queue):
-    client_info = get_client(message_queue)
-    while create_lobby(message_queue, client_info) != True:
+def queue():
+    client_info = get_client()
+    while create_lobby(client_info) != True:
         sleep(3)
 
-    change_arena_skin(message_queue, client_info)
+    change_arena_skin(client_info)
 
     sleep(3)
     while check_queue(client_info) is not True:
         sleep(5)
-        create_lobby(message_queue, client_info)
+        create_lobby(client_info)
         sleep(3)
-        start_queue(message_queue, client_info)
+        start_queue(client_info)
         sleep(1)
 
     in_queue = True
     time = 0
     while in_queue:
         if time % 60 == 0:
-            create_lobby(message_queue, client_info)
+            create_lobby(client_info)
             sleep(5)
-            start_queue(message_queue, client_info)
+            start_queue(client_info)
         accept_queue(client_info)
         if check_game_status(client_info):
             in_queue = False
