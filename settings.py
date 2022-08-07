91965d3bde92b8bdbc00b8994db674c1caf2975f
jfd02 <jerrydurkin02@gmail.com>
2022-08-07 16:37:48 -0400

    Fix comment

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@
 
 FORFEIT = False
 FORFEIT_TIME = 600  # Time in seconds
-LEAGUE_CLIENT_PATH = None # Replace None with your path. Ex: 'C:\\Riot Games\\League of Legends')
+LEAGUE_CLIENT_PATH = 'C:\\Riot Games\\League of Legends' # Replace None with your path. Ex: 'C:\\Riot Games\\League of Legends'
 TESSERACT_PATH = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
