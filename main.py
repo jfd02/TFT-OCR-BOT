06f88fea7f7839639d6b1d576ac5cc12c34a27a9
jfd02 <jerrydurkin02@gmail.com>
2022-08-07 13:28:03 -0400

    Main code cleanup

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 """
 Where the bot execution starts & contains the game loop that keeps the bot running indefinitely
 """
+
 import multiprocessing
-from ui import Ui
+from ui import UI
 import auto_queue
 from game import Game
 import settings
 
-def game_loop(message_queue):
+
+def game_loop(ui_queue: multiprocessing.Queue) -> None:
+    """Keeps the program running indefinetly by calling queue and game start in a loop"""
     while True:
         auto_queue.queue()
-        Game(message_queue)
+        Game(ui_queue)
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     if settings.LEAGUE_CLIENT_PATH is None:
-        raise Exception("No league client path specified. Please set the path in settings.py")
+        raise Exception(
+            "No league client path specified. Please set the path in settings.py")
     message_queue = multiprocessing.Queue()
-    overlay = Ui(message_queue)
+    overlay = UI(message_queue)
     game_thread = multiprocessing.Process(target=game_loop, args=(message_queue,))
 
     print("TFT OCR | https://github.com/jfd02/TFT-OCR-BOT")
