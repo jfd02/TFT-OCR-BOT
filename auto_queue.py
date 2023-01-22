2200c4d47cb6d902c36b9688db8b3839af1d7c5d
Sizzles <sizzles09@gmail.com>
2023-01-22 20:27:31 +1100

    Faster accept on queues, should pickup all orbs

--- auto_queue.py
+++ auto_queue.py
@@ -120,7 +120,7 @@ def queue() -> None:
     while in_queue:
         if time % 60 == 0:
             create_lobby(client_info)
-            sleep(5)
+            sleep(3)
             start_queue(client_info)
         accept_queue(client_info)
         if check_game_status(client_info):
