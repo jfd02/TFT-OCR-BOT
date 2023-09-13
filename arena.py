1cfa44c5f7b6ff58cef545acef0cb02c818aebbb
tony_sax <tony@wantenna.com>
2023-09-13 13:15:35 -0600

    AI - Also support placing Support items.

--- arena.py
+++ arena.py
@@ -284,16 +284,18 @@ class Arena:
             for index, _ in enumerate(self.items):
                 if self.items[index] is not None:
                     item = self.items[index]
+                    if item in game_assets.SUPPORT_ITEMS:
+                        print(f"    Found a Support Item: {item}")
                     if item in game_assets.MOGUL_ITEMS:
-                        print(f"    Found an Mogul Item: {item}")
+                        print(f"    Found a Mogul Item: {item}")
                     if item in game_assets.TRAIT_ITEMS:
-                        print(f"    Found an Trait Item: {item}")
+                        print(f"    Found a Trait Item: {item}")
                     if item in game_assets.RADIANT_ITEMS:
-                        print(f"    Found an Radiant Item: {item}")
+                        print(f"    Found a Radiant Item: {item}")
                     if item in game_assets.ORNN_ITEMS:
                         print(f"    Found an Ornn Item: {item}")
                     if item in game_assets.ZAUN_ITEMS:
-                        print(f"    Found an Zaun Item: {item}")
+                        print(f"    Found a Zaun Item: {item}")
                     if item in game_assets.CRAFTABLE_ITEMS_DICT:
                         print(f"    Found a Completed Item: {item}")
                     self.add_item_to_champs(index)
@@ -327,6 +329,13 @@ class Arena:
     def add_item_to_champ(self, item_index: int, champ: Champion) -> None:
         """Takes item index and champ and applies the item"""
         item = self.items[item_index]
+        if item in game_assets.SUPPORT_ITEMS:
+            if champ.name in comps.COMP and item in comps.COMP[champ.name]["support_items_to_accept"]:
+                print(f"        Attempting to add item {item} to {champ.name} because it is a Support item it accepts.")
+                arena_functions.move_item(screen_coords.ITEM_POS[item_index][0].get_coords(), champ.coords)
+                arena_functions.print_item_placed_on_champ(item, champ)
+                champ.completed_items.append(item)
+                self.items[self.items.index(item)] = None
         if item in game_assets.TRAIT_ITEMS:
             if champ.name in comps.COMP and item in comps.COMP[champ.name]["trait_items_to_accept"]:
                 print(f"        Attempting to add item {item} to {champ.name} because it is a Trait item it accepts.")
