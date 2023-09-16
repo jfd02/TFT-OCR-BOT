f879d4e8bc2ce6fcf1c5fa82174a90aa4e418610
tony_sax <tony@wantenna.com>
2023-09-15 20:58:45 -0600

    Champion - Add static method for creating a Champion object quickly.

--- champion.py
+++ champion.py
@@ -1,6 +1,35 @@
 """
 Contains all information related to an individual board slot used by the bot
 """
+import arena_functions
+import screen_coords
+from set_9_5 import game_assets
+
+
+def create_default_champion(champ_name: str, index: int, comp_to_play):
+    # Set default values if we don't want to use this champ in our comp.
+    items_to_build, build2, ornn_items, support_items, trait_items, zaun_items = [], [], [], [], [], []
+    size = 1
+    final_comp = False
+    units_current_item_count = arena_functions. \
+        count_number_of_item_slots_filled_on_unit_at_coords(screen_coords.BENCH_LOC[index].get_coords())
+    # If we actually plan on using this champ in our comp:
+    if champ_name in comp_to_play.comp:
+        items_to_build = comp_to_play.comp[champ_name]["items_to_build"].copy()
+        build2 = comp_to_play.comp[champ_name]["completed_items_to_accept"].copy(),
+        ornn_items = comp_to_play.comp[champ_name]["ornn_items_to_accept"].copy(),
+        support_items = comp_to_play.comp[champ_name]["support_items_to_accept"].copy(),
+        trait_items = comp_to_play.comp[champ_name]["trait_items_to_accept"].copy(),
+        zaun_items = comp_to_play.comp[champ_name]["zaun_items_to_accept"].copy(),
+        size = game_assets.CHAMPIONS[champ_name]["Board Size"]
+        final_comp = comp_to_play.comp[champ_name]["final_comp"]
+    # Create the Champion object.
+    champion_object = \
+        Champion(name=champ_name, coords=screen_coords.BENCH_LOC[index].get_coords(),
+                 item_slots_filled=units_current_item_count, build=items_to_build, build2=build2,
+                 ornn_items=ornn_items, support_items=support_items, trait_items=trait_items, zaun_items=zaun_items,
+                 slot=index, size=size, final_comp=final_comp)
+    return champion_object
 
 
 class Champion:
