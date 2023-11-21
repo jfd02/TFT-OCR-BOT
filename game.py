124e30c4c2cb4a51d28df61cc5a31e17d96b4292
Enterly <36875936+anthony5301@users.noreply.github.com>
2023-11-22 02:09:33 +0800

    Prevent move_unknown failed in 1-2 when an Anvil Portals is selected

--- game.py
+++ game.py
@@ -116,7 +116,7 @@ class Game:
         """Move unknown champion to board after first carousel"""
         print(f"\n[Second Round] {self.round}")
         self.message_queue.put("CLEAR")
-        self.arena.fix_bench_state()
+        self.arena.bench[arena_functions.bench_occupied_check().index(True)] = "?"
         self.arena.move_unknown()
         self.end_round_tasks()
 
