a571c784aeb0698bb054faa87eaf65775dbeab39
Enterly <36875936+anthony5301@users.noreply.github.com>
2023-11-23 02:50:53 +0800

    Clear anvil at 1-3

--- game.py
+++ game.py
@@ -144,6 +144,7 @@ class Game:
         if self.round == "1-3":
             sleep(1.5)
             self.arena.fix_unknown()
+            self.arena.clear_anvil()
             # self.arena.tacticians_crown_check() #not getting any item in set9 round 1-3, skipped
 
         self.arena.fix_bench_state()
